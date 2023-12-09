96eb50349b684ee76ba226609a1249b6fe3b1c59
Balulegend <balakrishnaramula@gmail.com>
2023-12-09 18:07:57 +0530

    findsum method is Added

--- src/com/edubridge/app1/util/Calc.java
+++ src/com/edubridge/app1/util/Calc.java
@@ -1,5 +1,10 @@
 package com.edubridge.app1.util;
 
 public class Calc {
-
+	public int findSquare(int num) {
+		return num*num;
+	}
+public int findsum(int num1, int num2) {
+	return num1+num2;
+}
 }
