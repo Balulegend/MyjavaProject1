32f168a0d36c123c1e18610de2a19aa49cf275c3
Balulegend <153358612+Balulegend@users.noreply.github.com>
2023-12-09 18:05:36 +0530

    Update Calc.java

--- src/com/edubridge/app1/util/Calc.java
+++ src/com/edubridge/app1/util/Calc.java
@@ -1,10 +1,6 @@
 package com.edubridge.app1.util;
 
 public class Calc {
-
-  public int findSquare(int num) {
-    return num*num;
-  }
     public int findCube(int num) {
     return num*num*num;
   }
